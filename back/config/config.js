c827f1d223df46ca6aef95beb894afbf6722e721
김민정 <mjkim@gaia3d.com>
2023-03-21 20:51:17 +0900

    feat: web page run
    npm run dev

--- back/config/config.js
+++ back/config/config.js
@@ -6,20 +6,23 @@ module.exports = {
     "password": process.env.MYSQL_PASSWORD,
     "database": "sleact",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 13306
   },
   "test": {
     "username": "root",
     "password": process.env.MYSQL_PASSWORD,
     "database": "sleact",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 13306
   },
   "production": {
     "username": "root",
     "password": process.env.MYSQL_PASSWORD,
     "database": "sleact",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 13306
   }
 }
